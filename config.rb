c9cecae549b8b4290bacd896ef93c2f34f1804a4
Kurt Zoglmann <zoglmannk@yahoo.com>
2015-08-01 19:23:24 -0500

    Setup 4 nodes w/ 3GB of ram

---
+++ config.rb
@@ -0,0 +1,9 @@
+# Size of the CoreOS cluster created by Vagrant
+$num_instances=4
+$vm_memory = 3000
+$vm_cpus = 2
+
+$expose_docker_tcp=2375
+
+# Official CoreOS channel from which updates should be downloaded
+$update_channel='stable'
